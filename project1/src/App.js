ff1aa51570d531cf32a1f4a05f79099f39b97ed6
mohammad mahdi <hasankhan.m@gmail.com>
2023-04-11 02:26:05 +0100

    state LIST

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,24 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+ const[courseList,changeText]=useState([]);
+
+const [insertText,settext]=useState("");
+
+const settext2=(event)=>{
+  settext(event.target.value)
+
+}
+
+const showText=()=>{
+  changeText(insertText)
+
+const newcourseList=[...courseList,insertText]
+changeText(newcourseList)
+
+};
+
+const deletecourse=(namecours)=>{
+const newdeletcours=courseList.filter(
+
+(courses)=>{
+
+  if (courses===namecours)return false
+  else return true
+}
+
+)
+changeText(newdeletcours)
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+  <div>
+
+   
+<input type="text" onChange={settext2}></input>
+<button  onClick={showText}>Insert</button>
+{courseList.map((courses)=>{
+return (
+<div>
+<h1>{courses}</h1> 
+<button onClick={()=>deletecourse(courses)}>Delete</button>
+</div>
+)
+
+})}
+
+  </div>
   );
 }
 
